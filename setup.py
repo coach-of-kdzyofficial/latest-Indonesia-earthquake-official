401a8c3b1e9cb03e8da7253f3a95d3a56bda914e
Niepep <niepep72@gmail.com>
2022-11-23 19:53:56 +0700

    upload package

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name = "latestIndonesiaearthquake-officiL",
+    version = "0.0.1",
+    authors = "Hanif Fadh",
+    author_email ="officialniepep@gmail.com",
+    description="This package will gate the latest earthquake from BMKG | Meteorological, Climatological, and Geophysical Agency.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+                "Programming Language :: Python :: 3",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent",]
+
+
+)
